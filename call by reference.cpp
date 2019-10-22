728f809a71967f1b50bceb00512e7d0e0b4bc46c
aashnamidha <54594946+aashnamidha@users.noreply.github.com>
2019-10-22 19:30:18 +0530

    Add files via upload

---
+++ call by reference.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void swap(int *x,int *y);
+int main()
+{
+	int x,y;
+	printf("Enter two numbers: ");
+	scanf("%d%d",&x,&y);
+	printf("\n Before swapping \n");
+	printf("Value of x= %d",x);
+	printf("\nValue of y= %d",y);
+	swap(&x,&y);
+	printf("\n\n After swapping \n");
+	printf("Value of x= %d",x);
+	printf("\nValue of y= %d",y);
+}
+void swap(int *x,int *y)
+{
+	int t;
+	t=*x;
+	*x=*y;
+	*y=t;
+	printf("\n\n Inside swap function \n");
+	printf("Value of x= %d",x);
+	printf("\nValue of y= %d",y);
+}
